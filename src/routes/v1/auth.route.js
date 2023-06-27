d660604b8618a1e97807fc54e646285c5434e7c2
dungdv <dvdung0000@gmail.com>
2023-06-27 16:34:03 +0700

    update role

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -10,7 +10,7 @@ router.post('/register', validate(authValidation.register), authController.regis
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
-router.route('/reset-password').post(auth('admin'), validate(authValidation.resetPass), authController.resetPass);
+router.route('/reset-password').post(auth('user'), validate(authValidation.resetPass), authController.resetPass);
 // router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
 // router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
 // router.post('/send-verification-email', auth(), authController.sendVerificationEmail);
